5885208689d1e0d6427694688c909261e1e533ea
siddharth raghunath <siddharthraghunath794@gmail.com>
2022-11-15 18:08:02 +0400

    Update initial_setup.sql

--- Section 3- Different Type of Tables/initial_setup.sql
+++ Section 3- Different Type of Tables/initial_setup.sql
@@ -1,13 +1,19 @@
-use role sysadmin ; 
-
-create database if not exists ecommerce_db;
-
-create schema if not exists ecommerce_liv;
-
-use schema ecommerce_db.ecommerce_liv;
-
-use warehouse compute_wh;
-
-create or replace table LINEITEM cluster by (L_SHIPDATE) as select * from "SNOWFLAKE_SAMPLE_DATA"."TPCH_SF1000"."LINEITEM" limit 2000000;
-
-create or replace table ORDERS as select * from "SNOWFLAKE_SAMPLE_DATA"."TPCH_SF1000"."ORDERS" ;
+    use role accountadmin; 
+     
+    CREATE DATABASE SNOWFLAKE_SAMPLE_DATA FROM SHARE SFC_SAMPLES.SAMPLE_DATA;
+     
+    GRANT IMPORTED PRIVILEGES ON DATABASE SNOWFLAKE_SAMPLE_DATA TO ROLE sysadmin;
+     
+    use role sysadmin ; 
+     
+    create database if not exists ecommerce_db;
+     
+    create schema if not exists ecommerce_liv;
+     
+    use schema ecommerce_db.ecommerce_liv;
+     
+    use warehouse compute_wh;
+     
+    create or replace table LINEITEM cluster by (L_SHIPDATE) as select * from "SNOWFLAKE_SAMPLE_DATA"."TPCH_SF1000"."LINEITEM" limit 2000000;
+     
+    create or replace table ORDERS as select * from "SNOWFLAKE_SAMPLE_DATA"."TPCH_SF1000"."ORDERS" ;
